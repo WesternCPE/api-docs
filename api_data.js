fff22529bd458750f40ece726df8b5bd3aa69a71
Bradford Knowlton <bradford.knowlton@westerncpe.com>
2021-04-26 11:37:26 -0600

    v2.1 commit

--- api_data.js
+++ api_data.js
@@ -1,5 +1,41 @@
 define({ "api": [
   {
+    "type": "delete",
+    "url": "/v2/user/:id",
+    "title": "Delete User information",
+    "name": "DeleteUser",
+    "group": "User",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Users unique ID.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "Result",
+            "description": "<p>Specify if the account was deleted.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "../wcpe-api/express/routes/users.js",
+    "groupTitle": "User"
+  },
+  {
     "type": "get",
     "url": "/v2/user/:id",
     "title": "Request User information",
